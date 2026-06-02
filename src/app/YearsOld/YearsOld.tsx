3958dff8872cdbb0792120ec2c5c615f4535742b
ssaprt <130990888+ssaprt@users.noreply.github.com>
2026-06-02 18:29:03 +0400

    update

--- src/app/YearsOld/YearsOld.tsx
+++ src/app/YearsOld/YearsOld.tsx
@@ -3,7 +3,6 @@ import StopIcon from "@/assets/icons/common/stop.svg";
 import { ButtonWithSvg } from "@/components/button/buttonWithSvg/buttonWithSvg";
 import { TitleSection } from "@/components/titles/TitleWithLogo/TitleSection";
 import { useRef } from "react";
-import { createPortal } from "react-dom";
 import styles from "./YearsOld.module.css";
 
 export const YearsOld = ({ onConfirm }: { onConfirm: () => void }) => {
@@ -13,7 +12,7 @@ export const YearsOld = ({ onConfirm }: { onConfirm: () => void }) => {
             onConfirm();
         }
     };
-    return createPortal(
+    return (
         <div
             className={styles.overlayAge}
             onAnimationEnd={handleEndAnimation}
@@ -46,7 +45,6 @@ export const YearsOld = ({ onConfirm }: { onConfirm: () => void }) => {
                     />
                 </div>
             </div>
-        </div>,
-        document.body,
+        </div>
     );
 };
